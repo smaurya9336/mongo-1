29ea5ac880844f6863c4502ef14341b0ace0e3dc
sarita maurya <you@example.com>
2024-07-01 20:14:52 +0530

    customer id (one to many)

---
+++ Models/customer.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+
+main()
+.then(()=> console.log("connection successful"))
+
+.catch((err) => console.log(err));
+
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo")
+}
+
+const orderSchema= new Schema({
+    item:String,
+    price:Number,
+})
+
+const customerSchema= new Schema({
+    
+
+})
+
+const Order= mongoose.model("Order",orderSchema);
+
+// const addOrders=async() =>{
+//     let res=await Order.insertMany([
+//         {
+//             item:"Smosa", price:12
+//         },
+//         {item:"Chips",price:10},
+//         {item:"Chocolate",price:40},
+//     ])
+//     console.log(res);
+// }
+addOrders();
